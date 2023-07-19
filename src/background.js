e06d9744a719e7d48bfdc8fbadf1929205a345a0
Baxter Addison <baxter@addisonbrown.com.au>
2023-07-19 21:51:05 +1000

    logo & changing settings will update orignal str

--- src/background.js
+++ src/background.js
@@ -12,37 +12,50 @@ chrome.contextMenus.create({
     contexts: ['selection'],
 })
 
-const config = {
-    decimals: 18,
-    displayDecimals: 18,
+let config = {
+    decimalsValue: 18,
+    displayDecimalsValue: 18,
 }
 chrome.storage.local.get(['decimals']).then((result) => {
-    config.decimals = +result.decimals
+    config.decimalsValue = +result.decimals
 })
 
 chrome.storage.local.get(['displayDecimals']).then((result) => {
-    config.displayDecimals = +result.displayDecimals
+    config.displayDecimalsValue = +result.displayDecimals
 })
 
+let preConverted
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === 'Wei to Eth') {
+        preConverted = info.selectionText
+        console.log(config.decimalsValue)
+        console.log(config.displayDecimalsValue)
+
         let convertedEth = convertWei(
             info.selectionText,
-            config.decimals,
-            config.displayDecimals
+            config.decimalsValue,
+            config.displayDecimalsValue
         )
+        console.log(preConverted)
+        console.log(convertedEth)
         chrome.storage.local.set({ Str: convertedEth })
+        chrome.storage.local.set({ conversionType: 'wei' })
+        chrome.storage.local.set({ preConverted: preConverted })
     }
 })
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === 'Hex Converter') {
+        preConverted = info.selectionText
         let convertedHex = convertHex(info.selectionText)
         let cutHex = convertWei(
             convertedHex,
-            config.decimals,
-            config.displayDecimals
+            config.decimalsValue,
+            config.displayDecimalsValue
         )
+        console.log(preConverted)
         chrome.storage.local.set({ Str: cutHex })
+        chrome.storage.local.set({ conversionType: 'hex' })
+        chrome.storage.local.set({ preConverted: preConverted })
     }
 })
--- src/popup.js
+++ src/popup.js
@@ -1,20 +1,20 @@
+const { convertWei, convertHex } = require('./converters')
+
 document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.local.get(['Str']).then((result) => {
         const storedStr = result.Str
-        const text = (document.getElementById('Eth').textContent = storedStr)
+        let text = (document.getElementById('Eth').textContent = storedStr)
     })
 
     let decimalsValue
     chrome.storage.local.get(['decimals']).then((result) => {
         decimalsValue = result.decimals
-        console.log(decimalsValue)
         document.getElementById('decimals').value = decimalsValue
     })
 
     let displayDecimalsValue
     chrome.storage.local.get(['displayDecimals']).then((result) => {
         displayDecimalsValue = result.displayDecimals
-        console.log(displayDecimalsValue)
         document.getElementById('displayDecimals').value = displayDecimalsValue
     })
 
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('submit', function (event) {
         event.preventDefault() // Prevent the default form submission
 
+        console.log('set')
         // decimals
         let decimalsValue = document.getElementById('decimals').value
         chrome.storage.local.set({ decimals: decimalsValue })
@@ -31,5 +32,33 @@ document.addEventListener('DOMContentLoaded', function () {
         let displayDecimalsValue =
             document.getElementById('displayDecimals').value
         chrome.storage.local.set({ displayDecimals: displayDecimalsValue })
+
+        // reconverting the number
+        let preConverted
+        let conversionType
+        chrome.storage.local.get(['preConverted']).then((result) => {
+            preConverted = result.preConverted
+            console.log(`1 ${preConverted}`)
+            chrome.storage.local.get(['conversionType']).then((result) => {
+                conversionType = result.conversionType
+                console.log(`1 ${conversionType}`)
+
+                if ((conversionType = 'wei')) {
+                    const convertedWei = convertWei(
+                        preConverted,
+                        decimalsValue,
+                        displayDecimalsValue
+                    )
+                    console.log(convertedWei)
+                    let text = (document.getElementById('Eth').textContent =
+                        convertedWei)
+                } else if ((conversionType = 'hex')) {
+                    let convertedHex = convertHex(preConverted)
+                    convertedHex = convertWei(convertedHex)
+                    let text = (document.getElementById('Eth').textContent =
+                        'convertedHex')
+                }
+            })
+        })
     })
 })
